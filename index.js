ea6b51cefeebfa0d68afa7ed655475ff85d05358
LucHighwalker <luc.highwalker@gmail.com>
2019-07-24 01:10:16 -0700

    - renamed keys lib to keyboard lib - moved over some functions

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const { keys, findKey } = require("./lib/keys");
+const { keyboards, getKeyStrokes, typeOutWord } = require("./lib/keyboard");
 
-const keyboard = "qwerty";
+const kb = "qwerty";
 
-getKeyStrokes = word => {
-	const strokes = [];
-	for (let i = 0; i < word.length; i++) {
-		strokes.push(findKey("qwerty", word[i]));
-	}
-	return strokes;
-};
-
-typeOutWord = strokes => {
-	word = "";
-	strokes.forEach(k => {
-		word = `${word}${keys[keyboard][k[0]][k[1]]}`;
-	});
-	return word;
-};
+console.log(keyboards)
 
 String.prototype.mistype = function(chance = 0.25) {
 	const keyStrokes = getKeyStrokes(this);
@@ -35,25 +21,24 @@ String.prototype.mistype = function(chance = 0.25) {
 						break;
 				}
 			}
-      newKey[1] = 0.5 > Math.random() ? key[1] - 1 : key[1] + 1;
-      newKey[1] = newKey[1] < 0 ? newKey[1] + 2 : newKey[1];
-      newKey[1] = newKey[1] > keys[keyboard][newKey[0]].length ? newKey[1] - 2 : newKey[1];
+			newKey[1] = 0.5 > Math.random() ? key[1] - 1 : key[1] + 1;
+			newKey[1] = newKey[1] < 0 ? newKey[1] + 2 : newKey[1];
+			newKey[1] =
+				newKey[1] > keyboards[kb][newKey[0]].length ? newKey[1] - 2 : newKey[1];
 			return newKey;
 		} else {
 			return key;
 		}
-  });
-  return typeOutWord(keyStrokes);
+	});
+	return typeOutWord(keyStrokes);
 };
 
 String.prototype.mistypeMany = function(count, chance = 0.25) {
-  const result = [];
-  for (let i = 0; i < count; i++) {
-    result.push(this.mistype(chance))
-  }
-  return result;
-}
+	const result = [];
+	for (let i = 0; i < count; i++) {
+		result.push(this.mistype(chance));
+	}
+	return result;
+};
 
 console.log("spelling".mistypeMany(100));
-
-// console.log(typeOutWord([[0, 2], [1, 6], [1, 1], [0, 5]]));
